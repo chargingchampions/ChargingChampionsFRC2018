b0a774bfdd31e530f8d5ec9ee93f64259b5d1aaf
Alex Li <li.alex13285@gmail.com>
2018-03-01 23:19:11 -0800

    Added some Timers

--- src/org/usfirst/frc/team6560/robot/commands/drive/DriveStraightToDistance.java
+++ src/org/usfirst/frc/team6560/robot/commands/drive/DriveStraightToDistance.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team6560.robot.commands.drive;
 
 import org.usfirst.frc.team6560.robot.Robot;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -12,9 +13,11 @@ public class DriveStraightToDistance extends Command {
 	double distance;
 	double speedToDrive;
 	double distanceToSlowDown = 6;
+	Timer shutdownTimer;
 
     public DriveStraightToDistance(double distanceIn, double speedIn) {
         requires(Robot.drive);
+        shutdownTimer = new Timer();
         distance = Math.abs(distanceIn);
         if (distance < 0) {
         	speedToDrive = -1*Math.abs(speedIn);
@@ -27,6 +30,8 @@ public class DriveStraightToDistance extends Command {
     	Robot.drive.gyro.reset();
     	Robot.drive.drive_enc_left.reset();
     	Robot.drive.drive_enc_right.reset();
+    	shutdownTimer.reset();
+    	shutdownTimer.start();
     }
 
     protected void execute() {
@@ -42,11 +47,13 @@ public class DriveStraightToDistance extends Command {
     	//TODO: add the left encoder for this too?
     	
     	//TODO: if the encoder is disconnected, you need to stop this command from running forever!!!
-    	return (Math.abs(Robot.drive.drive_enc_right.getDistance()) - distance) <= Robot.driveAbsTol || Math.abs(Robot.drive.drive_enc_right.getDistance()) >= distance;
+    	return (Math.abs(Robot.drive.drive_enc_right.getDistance()) - distance) <= Robot.driveAbsTol || Math.abs(Robot.drive.drive_enc_right.getDistance()) >= distance
+    			|| shutdownTimer.get() > 5.0;
     }
 
     protected void end() {
     	Robot.drive.stopDrive();
+    	shutdownTimer.stop();
     	System.out.println("The driveStraightToDistance has ended...");
     }
 
--- src/org/usfirst/frc/team6560/robot/commands/drive/TurnToAngle.java
+++ src/org/usfirst/frc/team6560/robot/commands/drive/TurnToAngle.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team6560.robot.commands.drive;
 
 import org.usfirst.frc.team6560.robot.Robot;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -12,15 +13,19 @@ public class TurnToAngle extends Command {
 	double angle; 
 	double speed;
 	double angleToSlowDown = 5;
+	Timer shutdownTimer;
 	
     public TurnToAngle(double angleToTurn, double speedToTurn) {
         requires(Robot.drive);
         angle = angleToTurn;
         speed = speedToTurn;
+        shutdownTimer = new Timer();
     }
 
     protected void initialize() {
     	Robot.drive.gyro.reset();
+    	shutdownTimer.reset();
+    	shutdownTimer.start();
     }
 
     protected void execute() {
@@ -44,12 +49,15 @@ public class TurnToAngle extends Command {
     	//TODO: if the gyro disconnects, you need to be able to stop this command from running forever!!!
         if(angle > 0)
         	return Math.abs(Robot.drive.getGyroAngle() - angle) <= Robot.driveRotateAbsTol || Robot.drive.getGyroAngle() >= angle;
-       	else
+        else if (angle < 0)
         	return Math.abs(Robot.drive.getGyroAngle() - angle) <= Robot.driveRotateAbsTol || Robot.drive.getGyroAngle() <= angle;
+        else
+        	return shutdownTimer.get() > 5.0; //Automatically stops this command after 5 seconds
     }
 
     protected void end() {
     	Robot.drive.stopDrive();
+    	shutdownTimer.stop();
     }
 
     protected void interrupted() {
