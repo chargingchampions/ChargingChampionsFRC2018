fc8cbc66a5aa6f1dff335f0a4a44898fb150179a
anish-neervannan <anish.neervannan@gmail.com>
2018-03-22 19:29:16 -0700

    Left auto is edited to work for scale. Drive Straight values need to be edited. We are not doing a Right Auto from now on ( this needs to be removed though).

--- src/org/usfirst/frc/team6560/robot/commands/auto/LeftTimeTime.java
+++ src/org/usfirst/frc/team6560/robot/commands/auto/LeftTimeTime.java
@@ -5,6 +5,8 @@ import org.usfirst.frc.team6560.robot.commands.cubeIntake.OpenIntakeArms;
 import org.usfirst.frc.team6560.robot.commands.drive.DriveStraightTime;
 import org.usfirst.frc.team6560.robot.commands.drive.TurnToAngleTime;
 import org.usfirst.frc.team6560.robot.commands.grabber.RotateGrabberTime;
+import org.usfirst.frc.team6560.robot.commands.cubeIntake.ShootCube;
+//import org.usfirst.frc.team6560.robot.commands.cubeIntake.StopIntake;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
@@ -15,7 +17,7 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
 public class LeftTimeTime extends CommandGroup {
 
 	public LeftTimeTime(String givenData, double driveScalar, double rotateScalar) {
-		
+		System.out.println(givenData);
 		char switchPos = 0;
 		char scalePos = 0;
 		try {
@@ -24,22 +26,43 @@ public class LeftTimeTime extends CommandGroup {
 		} catch (NullPointerException npe) {
 			System.out.println("Game data Nonexistent!");
 		}
-		if (switchPos == 'L') {
-			System.out.println("Going to left switch from left station");
+		if (scalePos == 'L') {
+			System.out.println("Going to left scale from left station");
 			addParallel(new RotateArmTime(0.8, 0.7));
 			addParallel(new RotateGrabberTime(0.4, 0.7));
-			addSequential(new DriveStraightTime(2.5*driveScalar, 0.7));
+			addSequential(new DriveStraightTime(1.9*driveScalar, 0.7));
 			addSequential(new WaitCommand(0.5));
-			addSequential(new TurnToAngleTime(0.80*rotateScalar, 0.85));
+			addSequential(new TurnToAngleTime(0.60*rotateScalar, 0.7));
 			addSequential(new WaitCommand(0.5));
-			addSequential(new DriveStraightTime(0.8*driveScalar, 0.7));
-			addSequential(new OpenIntakeArms());
-			addSequential(new DriveStraightTime(0.5*driveScalar, -0.7));
-			addSequential(new TurnToAngleTime(0.80*rotateScalar, -0.85));
-		} else if (scalePos == 'L'){
-			System.out.println("I guess the game just hates us...");
-		} else {
-			System.out.println("I guess the game just hates us...");
+			addSequential(new RotateArmTime(0.8, 0.7));
+			addSequential(new RotateGrabberTime(0.55, 0.7));
+			addSequential(new DriveStraightTime(0.4*driveScalar, 0.6));
+			addSequential(new WaitCommand(0.5));
+			//addSequential(new OpenIntakeArms());
+			addSequential(new ShootCube(1.0));
+			addSequential(new DriveStraightTime(0.4*driveScalar, -0.6));
+		} else if (scalePos == 'R') {
+			System.out.println("Going to right scale from left station");
+			addParallel(new RotateArmTime(0.8, 0.7));
+			addParallel(new RotateGrabberTime(0.4, 0.7));
+			addSequential(new DriveStraightTime(1.6*driveScalar, 0.7));
+			addSequential(new WaitCommand(0.5));
+			addSequential(new TurnToAngleTime(0.60*rotateScalar, 0.7));
+			addSequential(new WaitCommand(0.5));
+			addSequential(new DriveStraightTime(3.5*driveScalar, 0.7)); // from left to right
+			addSequential(new WaitCommand(0.5));
+			addSequential(new TurnToAngleTime(0.60*rotateScalar, -0.7));
+			addSequential(new WaitCommand(0.5));
+			addSequential(new DriveStraightTime(0.4*driveScalar, 0.7)); // from platform zone to scale
+			addSequential(new WaitCommand(0.5));
+			addSequential(new TurnToAngleTime(0.60*rotateScalar, -0.7)); // turn to face scale 
+			addSequential(new RotateArmTime(0.8, 0.7));
+			addSequential(new RotateGrabberTime(0.55, 0.7));
+			addSequential(new DriveStraightTime(0.4*driveScalar, 0.6));
+			addSequential(new WaitCommand(0.5));
+			//addSequential(new OpenIntakeArms());
+			addSequential(new ShootCube(1.0));
+			addSequential(new DriveStraightTime(0.4*driveScalar, -0.6));
 		}
 	}
 }
--- src/org/usfirst/frc/team6560/robot/commands/cubeIntake/ShootCube.java
+++ src/org/usfirst/frc/team6560/robot/commands/cubeIntake/ShootCube.java
@@ -2,29 +2,48 @@ package org.usfirst.frc.team6560.robot.commands.cubeIntake;
 
 import org.usfirst.frc.team6560.robot.Robot;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class ShootCube extends Command {
+	
+	Timer shutdownTimer = null;
+	double time;
 
 	public ShootCube() {
 		requires(Robot.cubeIntake);
 	}
-
-	protected void initialize() {
+	
+	public ShootCube(double time) {
+		requires(Robot.cubeIntake);
+		shutdownTimer = new Timer();
+		this.time = time;
 	}
 
-	protected void execute() {
+	protected void initialize() {
+		if (shutdownTimer != null) {
+			shutdownTimer.start();
+		}
 		Robot.cubeIntake.shootCube(0.6);
 	}
 
+	protected void execute() {
+	}
+
 	protected boolean isFinished() {
-		return false;
+		if (shutdownTimer != null) {
+			return shutdownTimer.get() >= time;
+		} else
+			return false;
 	}
 
 	protected void end() {
+		if (shutdownTimer != null) {
+			shutdownTimer.stop();
+		}
 		Robot.cubeIntake.shootCube(0);
 	}
 
